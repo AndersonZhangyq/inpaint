c04e74cff1fc223d00375ba72d2e4d22bcebad4e
Anderson <andersonzhangyq@gmail.com>
2019-11-13 16:08:23 +0800

    deep-image-prior

--- pytorch-lightning-seed/research_seed/deep_image_prior/mnist.py
+++ pytorch-lightning-seed/research_seed/deep_image_prior/mnist.py
@@ -8,37 +8,137 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 from argparse import ArgumentParser
+import torch.nn as nn
 
 import pytorch_lightning as pl
+from research_seed.common import *
+from research_seed.dataset import *
 
 
 class DeepImagePrior(pl.LightningModule):
-
     def __init__(self, hparams):
         super(DeepImagePrior, self).__init__()
         # not the best model...
         self.hparams = hparams
         self.l1 = torch.nn.Linear(28 * 28, 10)
+        self.data_root = "data"
+        num_input_channels = 2
+        num_output_channels = 3
+        num_channels_down = [16, 32, 64, 128, 128]
+        num_channels_up = [16, 32, 64, 128, 128]
+        num_channels_skip = [4, 4, 4, 4, 4]
+        filter_size_down = 3
+        filter_size_up = 3
+        filter_skip_size = 1
+        need_sigmoid = True
+        need_bias = True
+        pad = 'zero'
+        upsample_mode = 'nearest'
+        downsample_mode = 'stride'
+        act_fun = 'LeakyReLU'
+        need1x1_up = True
+        assert len(num_channels_down) == len(
+            num_channels_up) == len(num_channels_skip)
+
+        n_scales = len(num_channels_down)
+
+        if not (isinstance(upsample_mode, list) or isinstance(upsample_mode, tuple)):
+            upsample_mode = [upsample_mode]*n_scales
+
+        if not (isinstance(downsample_mode, list)or isinstance(downsample_mode, tuple)):
+            downsample_mode = [downsample_mode]*n_scales
+
+        if not (isinstance(filter_size_down, list) or isinstance(filter_size_down, tuple)):
+            filter_size_down = [filter_size_down]*n_scales
+
+        if not (isinstance(filter_size_up, list) or isinstance(filter_size_up, tuple)):
+            filter_size_up = [filter_size_up]*n_scales
+        
+        last_scale = n_scales - 1 
+
+        cur_depth = None
+
+        input_depth = num_input_channels
+        
+        model = nn.Sequential()
+        model_tmp = model
+
+        for i in range(len(num_channels_down)):
+
+            deeper = nn.Sequential()
+            skip = nn.Sequential()
+
+            if num_channels_skip[i] != 0:
+                model_tmp.add(Concat(1, skip, deeper))
+            else:
+                model_tmp.add(deeper)
+            
+            model_tmp.add(bn(num_channels_skip[i] + (num_channels_up[i + 1] if i < last_scale else num_channels_down[i])))
+
+            if num_channels_skip[i] != 0:
+                skip.add(conv(input_depth, num_channels_skip[i], filter_skip_size, bias=need_bias, pad=pad))
+                skip.add(bn(num_channels_skip[i]))
+                skip.add(act(act_fun))
+                
+            # skip.add(Concat(2, GenNoise(nums_noise[i]), skip_part))
+
+            deeper.add(conv(input_depth, num_channels_down[i], filter_size_down[i], 2, bias=need_bias, pad=pad, downsample_mode=downsample_mode[i]))
+            deeper.add(bn(num_channels_down[i]))
+            deeper.add(act(act_fun))
+
+            deeper.add(conv(num_channels_down[i], num_channels_down[i], filter_size_down[i], bias=need_bias, pad=pad))
+            deeper.add(bn(num_channels_down[i]))
+            deeper.add(act(act_fun))
+
+            deeper_main = nn.Sequential()
+
+            if i == len(num_channels_down) - 1:
+                # The deepest
+                k = num_channels_down[i]
+            else:
+                deeper.add(deeper_main)
+                k = num_channels_up[i + 1]
+
+            deeper.add(nn.Upsample(scale_factor=2, mode=upsample_mode[i]))
+
+            model_tmp.add(conv(num_channels_skip[i] + k, num_channels_up[i], filter_size_up[i], 1, bias=need_bias, pad=pad))
+            model_tmp.add(bn(num_channels_up[i]))
+            model_tmp.add(act(act_fun))
+
+
+            if need1x1_up:
+                model_tmp.add(conv(num_channels_up[i], num_channels_up[i], 1, bias=need_bias, pad=pad))
+                model_tmp.add(bn(num_channels_up[i]))
+                model_tmp.add(act(act_fun))
+
+            input_depth = num_channels_down[i]
+            model_tmp = deeper_main
+
+        model.add(conv(num_channels_up[0], num_output_channels, 1, bias=need_bias, pad=pad))
+        if need_sigmoid:
+            model.add(nn.Sigmoid())
+        
+        self.model = model
 
     def forward(self, x):
-        return torch.relu(self.l1(x.view(x.size(0), -1)))
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
         # REQUIRED
-        x, y = batch
-        y_hat = self.forward(x)
-        return {'loss': F.cross_entropy(y_hat, y)}
+        noise, origin, mask = batch
+        predicted = self.forward(noise)
+        return {'loss': F.mse_loss(predicted * mask, origin * mask)}
 
-    def validation_step(self, batch, batch_idx):
-        # OPTIONAL
-        x, y = batch
-        y_hat = self.forward(x)
-        return {'val_loss': F.cross_entropy(y_hat, y)}
+    # def validation_step(self, batch, batch_idx):
+    #     # OPTIONAL
+    #     x, y = batch
+    #     y_hat = self.forward(x)
+    #     return {'val_loss': F.cross_entropy(y_hat, y)}
 
-    def validation_end(self, outputs):
-        # OPTIONAL
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        return {'avg_val_loss': avg_loss}
+    # def validation_end(self, outputs):
+    #     # OPTIONAL
+    #     avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+    #     return {'avg_val_loss': avg_loss}
 
     def configure_optimizers(self):
         # REQUIRED
@@ -48,17 +148,17 @@ class DeepImagePrior(pl.LightningModule):
     @pl.data_loader
     def train_dataloader(self):
         # REQUIRED
-        return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=self.hparams.batch_size)
+        return DataLoader(DeepImagePriorDataset(self.data_root), batch_size=1)
 
-    @pl.data_loader
-    def val_dataloader(self):
-        # OPTIONAL
-        return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=self.hparams.batch_size)
+    # @pl.data_loader
+    # def val_dataloader(self):
+    #     # OPTIONAL
+    #     return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=1)
 
-    @pl.data_loader
-    def test_dataloader(self):
-        # OPTIONAL
-        return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=self.hparams.batch_size)
+    # @pl.data_loader
+    # def test_dataloader(self):
+    #     # OPTIONAL
+    #     return DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor()), batch_size=1)
 
     @staticmethod
     def add_model_specific_args(parent_parser):
@@ -68,10 +168,9 @@ class DeepImagePrior(pl.LightningModule):
         # MODEL specific
         parser = ArgumentParser(parents=[parent_parser])
         parser.add_argument('--learning_rate', default=0.02, type=float)
-        parser.add_argument('--batch_size', default=32, type=int)
+        parser.add_argument('--batch_size', default=1, type=int)
 
         # training specific (for this model)
         parser.add_argument('--max_nb_epochs', default=2, type=int)
 
         return parser
-
